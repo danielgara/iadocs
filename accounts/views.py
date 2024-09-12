6b15e59a94909a2fb5c59329f684dd002ed27eae
Daniel Correa <yo@danielgara.com>
2024-09-12 15:07:10 -0500

    imp: login

---
+++ accounts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('home.index')
+    
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html', {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+        if user is None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html', {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
