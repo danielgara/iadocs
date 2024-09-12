07a5a6f93f720725e256d5ef80d537c1a27c2154
Daniel Correa <yo@danielgara.com>
2024-09-12 14:57:38 -0500

    imp: base

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Analizador de Convocatorias'
+    return render(request, 'home/index.html', {'template_data': template_data})
+
+def about(request):
+    template_data = {}
+    template_data['title'] = 'Acerca'
+    return render(request, 'home/about.html', {'template_data': template_data})
